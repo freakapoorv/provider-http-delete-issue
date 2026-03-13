05f79acc0add1a2375a237f5b4f3b9c97f1aeeab
freakapoorv <125556268+freakapoorv@users.noreply.github.com>
2026-03-13 18:21:33 +0530

    Update request.go
    testing deletion
    
    Signed-off-by: freakapoorv <125556268+freakapoorv@users.noreply.github.com>

--- internal/controller/cluster/request/request.go
+++ internal/controller/cluster/request/request.go
@@ -187,7 +187,7 @@ func (c *external) Observe(ctx context.Context, mg resource.Managed) (managed.Ex
 	if meta.WasDeleted(mg) {
 		c.logger.Debug("Request is being deleted, skipping observation")
 		return managed.ExternalObservation{
-			ResourceExists: false,
+			ResourceExists: true,
 		}, nil
 	}
 
@@ -196,7 +196,7 @@ func (c *external) Observe(ctx context.Context, mg resource.Managed) (managed.Ex
 	observeRequestDetails, err := request.IsUpToDate(svcCtx, crCtx)
 	if err != nil && err.Error() == observe.ErrObjectNotFound {
 		return managed.ExternalObservation{
-			ResourceExists: true,
+			ResourceExists: false,
 		}, nil
 	}
 
